11ab8027a73172a96ecec901fca6d936541bb4a3
javierc22 <javier.ec.22@gmail.com>
2020-06-20 16:12:27 -0400

    Agregando Emotion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F574E 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #FFF;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
 
 function App() {
+  const consultarAPI = () => {
+    console.log("consultando...")
+  }
+
   return (
-    <h1>Breaking Bad</h1>
+    <Contenedor>
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   );
 }
 
